78893b341dbf7585bf55eaf348b8e4a721cadfe4
Uma Shankar <shankar.uma35@gmail.com>
2017-09-02 17:33:52 +0530

    Boot Program sample

---
+++ fundooPay/src/main/java/com/bridgelabz/fundoopay/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.fundoopay;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	List<Customer> findByLastName(String lastName);
+
+}
